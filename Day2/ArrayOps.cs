1762d884277022cfe3f21dd7b05052dcd6263e47
S2 <shashankshowstoper@gmail.com>
2020-11-12 13:03:28 +0530

    Update ArrayOps.cs

--- Day2/ArrayOps.cs
+++ Day2/ArrayOps.cs
@@ -7,3 +7,25 @@ element 15, computes the average and stores it at element 16 and
 identifies the smallest value from the array and stores it at element 17.
 */
 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class SumOfArray{
+   public static void main(){
+      int []A = {3,2,4,5,6,4,5,7,3,2,3,4,7,1,2,0,0,0};
+      int sum = 0, i, min = A[0];
+      for(i=0; i<15; i++){
+          sum += i;
+          if(A[i]<=min)
+            min=A[i];
+      }
+      A[15] = sum;
+      A[16] = sum/15;
+      A[17] = min;
+      Console.WriteLine("The array is:");
+      for(i=0; i<A.Length;i++)
+        Console.Write(A[i] + ", ");
+   }
+}
+
