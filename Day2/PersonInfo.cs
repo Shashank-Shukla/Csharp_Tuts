a8e5994f336109ba9cca8da0ccb2886372b7447d
S2 <shashankshowstoper@gmail.com>
2020-11-12 13:04:52 +0530

    Create PersonInfo.cs

---
+++ Day2/PersonInfo.cs
@@ -0,0 +1,10 @@
+/*
+In this exercise you are going to write a Person class. 
+A person has several attributes that include name, date of birth, address and marital status (single, married, divorced or widowed). 
+Create a four parameter constructor to initialise the fields 
+		Add properties for the fields 
+		Write a method GetAge() to return the age of the person 
+		Write a method CanMarry() to return true or false if a person can get married (people can get married over the age of 18 if they are not already maried) 
+		Write a method PrintDetails() to display the persons details to the screen (format = Fred lives at 21 Lancaster road, born on 12/12/1980, single, 20 years old and can marry)
+*/
+
