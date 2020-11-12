de5598d38d861b729aff1bbdbbe73958899df58a
S2 <shashankshowstoper@gmail.com>
2020-11-12 12:19:07 +0530

    Create ArrayOps.cs

---
+++ Day2/ArrayOps.cs
@@ -0,0 +1,9 @@
+/*
+Write a program that allows you to create an integer array of 
+18 elements with the following values: 
+int A[]={3,2,4,5,6,4,5,7,3,2,3,4,7,1,2,0,0,0}. 
+The program computes the sum of element 0 to 14 and stores it at 
+element 15, computes the average and stores it at element 16 and 
+identifies the smallest value from the array and stores it at element 17.
+*/
+
